6a6056bd734b422c2b8205f5b8fa5830641069dc
Yunho Ding <gurumindc123@gmail.com>
2025-03-10 17:55:05 +0000

    Refactor avatar component formatting and update package dependencies

--- frontend/components/ui/avatar.tsx
+++ frontend/components/ui/avatar.tsx
@@ -15,7 +15,7 @@ const Avatar = React.forwardRef<
     ref={ref}
     className={cn(
       "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
-      className,
+      className
     )}
     {...props}
   />
