2971f24a22313a3c83e39d84e37840e39e4a375b
Yunho Ding <gurumindc123@gmail.com>
2025-03-10 04:04:18 +0000

    Update devcontainer configuration, add Docker Compose file, and create frontend run scripts

--- run_frontend.ps1
+++ run_frontend.ps1
@@ -1,7 +1,5 @@
-#!/bin/bash
-
 # Build docker image with volume
 docker build -f ./frontend/Dockerfile -t nasat .
 
 # Run docker container with volume
-docker run -it -p 19000:19000 -v "$(pwd)/frontend:/app" nasat
+docker run -it -p 19000:19000 -v "${PWD}\frontend:/app" nasat
