45203e9ef50287f28af3edb971b58e31f98d2694
Yunho Ding <gurumindc123@gmail.com>
2025-03-10 17:28:20 +0000

    format the avatar.tsx file

--- frontend/components/ui/avatar.tsx
+++ frontend/components/ui/avatar.tsx
@@ -1,5 +1,6 @@
 import * as AvatarPrimitive from "@rn-primitives/avatar";
 import * as React from "react";
+
 import { cn } from "~/lib/utils";
 
 const AvatarPrimitiveRoot = AvatarPrimitive.Root;
@@ -14,7 +15,7 @@ const Avatar = React.forwardRef<
     ref={ref}
     className={cn(
       "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
-      className
+      className,
     )}
     {...props}
   />
