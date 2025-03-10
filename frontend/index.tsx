a2e50ae30e81889e54bad92c4ac62319e2f50793
Yunho Ding <75521446+yunho7687@users.noreply.github.com>
2025-03-10 20:57:37 +0800

    Add new images and utility functions; remove deprecated files and components (#7)
    * Add new UI components and utilities for improved frontend structure
    
    * Add new images and utility functions; remove deprecated files and components

---
+++ frontend/index.tsx
@@ -0,0 +1,14 @@
+import '@expo/metro-runtime';
+
+import { registerRootComponent } from 'expo';
+import { ExpoRoot } from 'expo-router';
+
+// https://docs.expo.dev/router/reference/troubleshooting/#expo_router_app_root-not-defined
+
+// Must be exported or Fast Refresh won't update the context
+export function App() {
+  const ctx = (require as any).context('./app');
+  return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
