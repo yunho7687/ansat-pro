56816321b1c26c59c6f56cb5c14af9208d5fd91d
Yunho Ding <75521446+yunho7687@users.noreply.github.com>
2025-03-10 12:21:21 +0800

    Update devcontainer setup with Docker Compose and frontend scripts (#5)
    * Update devcontainer configuration, add Docker Compose file, and create frontend run scripts
    
    * Update Docker configuration: modify frontend Dockerfile path and add backend Dockerfile
    
    * Fix Dockerfile paths and update container naming in frontend scripts

--- run_frontend.ps1
+++ run_frontend.ps1
@@ -1,7 +1,5 @@
-#!/bin/bash
-
 # Build docker image with volume
 docker build -f ./docker/frontend/Dockerfile -t frontend .
 
 # Run docker container with volume
-docker run -it -p 19000:19000 -v "$(pwd)/frontend:/app" frontend
+docker run -it -p 19000:19000 -v "${PWD}\frontend:/app" frontend
